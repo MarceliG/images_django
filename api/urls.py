31b971e1d75e00ad10739f9773ce69f91e62eae0
MarceliG <marcel9817@gmail.com>
2022-06-15 21:47:33 +0200

    add_images_to_RFD

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
+router.register(r"images", ImageViewSet, 'images')
 
 
 urlpatterns = [
